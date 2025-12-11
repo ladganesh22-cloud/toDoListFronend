038dbe54a8228f238e844adc87da8e4d05be016f
24gbl <ganesh.l@iksula.com>
2025-12-11 21:25:38 +0530

    change-localhost-to-render-host

--- src/hooks/useFetchToDoList.jsx
+++ src/hooks/useFetchToDoList.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8081/todolists';
+const API_URL = 'https://todolistbackend-yrq1.onrender.com/todolists';
 
 const useFetchToDoList = () => {
   const [toDoList, setToDoList] = useState([]);
